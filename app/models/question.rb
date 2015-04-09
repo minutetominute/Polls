5cf172a667f6430f203193d771d4f0dc619c61fe
lonewarrior556 <lonewarrior556@yahoo.com>
2015-04-09 15:11:50 -0400

    Start question result methods

--- app/models/question.rb
+++ app/models/question.rb
@@ -11,4 +11,25 @@ class Question < ActiveRecord::Base
     class_name: "Poll",
     foreign_key: :poll_id,
     primary_key: :id
+
+  has_many :responses, through: :answer_choices, source: :responses
+
+  def results
+    answer_choices.each_with_object({}) do |choice, result|
+      result[choice.body] = choice.responses.count
+    end
+  end
+
+  def better_results
+    answer_choices.includes(:responses).each_with_object({}) do |choice, result|
+      result[choice.body] = choice.responses.length
+    end
+  end
+
+  def dank_results
+    results = question.find_by_sql(<<-SQL, )
+    
+
+    SQL
+  end
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,6 +1,8 @@
 class Response < ActiveRecord::Base
   validates :user_id, presence: true
   validates :answer_choice_id, presence: true
+  validate :respondent_has_not_already_answered_question
+  validate :respondent_is_not_question_author
 
   belongs_to :answer_choice,
     class_name: "AnswerChoice",
@@ -15,6 +17,24 @@ class Response < ActiveRecord::Base
   has_one :question, through: :answer_choice, source: :question
 
   def sibling_responses
-    question.responses
+    # question.responses.where.not(id: self.id)
+    question.responses.where(<<-SQL, id: self.id)
+      :id IS NULL
+      OR
+      responses.id != :id
+    SQL
+  end
+
+  private
+  def respondent_has_not_already_answered_question
+    if sibling_responses.exists?(self.user_id)
+      errors[:response] << "already answered"
+    end
+  end
+
+  def respondent_is_not_question_author
+    if question.poll.author_id == self.user_id
+      errors[:response] << "stahp plz go"
+    end
   end
 end
